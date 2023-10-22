a31a15426429e025ecf1279b85103d2c804d08b3
Allan Estrada <allan.estrada9@gmail.com>
2023-10-21 18:24:40 -0600

    Agregando Archivos de GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/react-tasks-application/'
-//   // base: 'https://DRAC-09.github.io/react-tasks-application'
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/react-tasks-application/",
+  // base: 'https://DRAC-09.github.io/react-tasks-application'
+})
